a125acf459312da8a9b0770fff4d048552fed0f4
= <julianstampfli4@gmail.com>
2019-05-19 18:03:10 +0200

    cleanup: multiple renames. Config cleanup

--- config/db_config.py
+++ config/db_config.py
@@ -1,16 +1,29 @@
 import hashlib
 
 
-class DbConfig:
+class SqLiteDbConfig:
     def __init__(self, config):
-        self.host = config['host']
-        self.port = config['port']
-        self.dbname = config['dbname']
-        self.user = config['user']
-        self.password = config['password']
+        keys = config.keys()
+        self.type = 'SqLite'
+        self.filename = config['filename'] if 'filename' in keys else 'fids_db.db'
 
     def __repr__(self):
-        return ("DbConfig("
+        return ("SqLiteDbConfig("
+                f"filename={self.filename},)")
+
+
+class RemoteDbConfig:
+    def __init__(self, config):
+        keys = config.keys()
+        self.type = 'Remote'
+        self.host = config['host'] if 'host' in keys else ""
+        self.port = config['port'] if 'port' in keys else ""
+        self.dbname = config['dbname'] if 'dbname' in keys else ""
+        self.user = config['user'] if 'user' in keys else ""
+        self.password = config['password'] if 'password' in keys else ""
+
+    def __repr__(self):
+        return ("RemoteDbConfig("
                 f"host={self.host},"
                 f"port={self.port},"
                 f"dbname={self.dbname},"
