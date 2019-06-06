bf97a8f5d1f723675c36f518641c7cf1214e2a59
= <julianstampfli4@gmail.com>
2019-06-06 19:59:15 +0200

    added alerts squashing for quick access in investigator

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,5 @@
 from .scan_config import ScanConfig
-from .detection_config import DetectionConfig
 from .db_config import SqLiteDbConfig, RemoteDbConfig
+from .investigator_config import InvestigatorConfig, InvestigationsConfig, RuleConfig
 from .main_config import Config
 name = 'config'
--- config/investigator_config.py
+++ config/investigator_config.py
@@ -3,9 +3,10 @@ class InvestigatorConfig:
         keys = config.keys()
         self._add_default_rules()
         self._parse_rules(config['rules'] if 'rules' in keys else None)
+        self._parse_investigations(
+            config['investigations'] if 'investigations' in keys else None)
         self.same_config = config['same_config'] if 'same_config' in keys else True
-        self.greater = config['greater'] if 'greater' in keys else []
-        self.equal = config['equal'] if 'equal' in keys else []
+        self.validation_run = config['validation_run'] if 'validation_run' in keys else ''
 
     def _add_default_rules(self):
         self.rules = []
@@ -25,7 +26,7 @@ class InvestigatorConfig:
         self.rules.append(RuleConfig(
             name='c', equal=['meta_changed_time', 'meta_changed_time_nano']))
         self.rules.append(RuleConfig(name='S', greater=['meta_size']))
-        self.rules.append(RuleConfig(name='', equal=['']))
+        self.rules.append(RuleConfig(name='empty'))
 
     def _parse_rules(self, rules_config):
         if rules_config is None:
@@ -33,17 +34,69 @@ class InvestigatorConfig:
         for rule in rules_config:
             self.rules.append(RuleConfig(config=rule))
 
+    def _parse_investigations(self, investigation_config):
+        self.investigations = []
+        if investigation_config is None:
+            return
+        for inestigation in investigation_config:
+            self.investigations.append(
+                InvestigationsConfig(config=inestigation))
+
+    def expand_rules(self):
+        for rule in self.rules:
+            rule.clean_greater_and_equal()
+            rule.expand_rules(self.rules)
+        for investigation in self.investigations:
+            investigation.validate_rules(self.rules)
+
+    def __repr__(self):
+        return ("InvestigatorConfig("
+                f"rules={self.rules},"
+                f"investigations={self.investigations},"
+                f"same_config={self.same_config},"
+                f"validation_run={self.validation_run},"
+                ")")
+
 
 class InvestigationsConfig:
-    pass
+    def __init__(self, config):
+        keys = config.keys()
+        self.paths = config['paths'] if 'paths' in keys else []
+        self.fileregexwhitelist = config['fileregexwhitelist'] if 'fileregexwhitelist' in keys else ''
+        self.fileregexblacklist = config['fileregexblacklist'] if 'fileregexblacklist' in keys else ''
+        self.rules = config['rules'] if 'rules' in keys else []
+        self.greater = []
+        self.equal = []
+
+    def validate_rules(self, all_rules):
+        if not self.rules:
+            return
+        for rule_name in self.rules:
+            rule = next(rule for rule in all_rules if rule.name == rule_name)
+            for grt in rule.greater:
+                if not grt in self.greater:
+                    self.greater.append(grt)
+            for eq in rule.equal:
+                if not eq in self.equal and not eq in self.greater:
+                    self.equal.append(eq)
+
+    def __repr__(self):
+        return ("InvestigationsConfig("
+                f"paths={self.paths},"
+                f"fileregexwhitelist={self.fileregexwhitelist},"
+                f"fileregexblacklist={self.fileregexblacklist},"
+                f"rules={self.rules},"
+                f"greater={self.greater},"
+                f"equal={self.equal},"
+                ")")
 
 
 class RuleConfig:
-    def __init__(self, config=None, name='', regex='', included=[], greater=[], equal=[]):
+    def __init__(self, config=None, name='', regex='', rules=[], greater=[], equal=[]):
         if config is not None:
             self._by_config(config)
         elif name:
-            self._by_values(name, regex=regex, included=included,
+            self._by_values(name, regex=regex, rules=rules,
                             greater=greater, equal=equal)
 
     def _by_config(self, config):
@@ -51,14 +104,42 @@ class RuleConfig:
         if(not 'name' in keys):
             return
         self.name = config['name']
-        self.regex = config['regex'] if 'regex' in keys else ''
-        self.inlcuded = config['inlcuded'] if 'inlcuded' in keys else []
+        self.rules = config['rules'] if 'rules' in keys else []
         self.greater = config['greater'] if 'greater' in keys else []
         self.equal = config['equal'] if 'equal' in keys else []
 
-    def _by_values(self, name, regex='', included=[], greater=[], equal=[]):
+    def _by_values(self, name, regex='', rules=[], greater=[], equal=[]):
         self.name = name
         self.regex = regex
-        self.inlcuded = included
+        self.rules = rules
         self.greater = greater
         self.equal = equal
+
+    def clean_greater_and_equal(self):
+        for eq in self.equal:
+            if eq in self.greater:
+                self.equal.remove(eq)
+
+    def expand_rules(self, all_rules):
+        if not self.rules:
+            return
+        for rule_name in self.rules:
+            self.rules.remove(rule_name)
+            rule = next(rule for rule in all_rules if rule.name == rule_name)
+            rule.expand_rules(all_rules)
+            for grt in rule.greater:
+                if not grt in self.greater:
+                    self.greater.append(grt)
+                if grt in self.equal:
+                    self.equal.remove(grt)
+            for eq in rule.equal:
+                if not eq in self.equal and not eq in self.greater:
+                    self.equal.append(eq)
+
+    def __repr__(self):
+        return ("RuleConfig("
+                f"name={self.name},"
+                f"rules={self.rules},"
+                f"greater={self.greater},"
+                f"equal={self.equal},"
+                ")")
